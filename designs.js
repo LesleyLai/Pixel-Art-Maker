9c419e33896779172c2b522fa5521aa27db73ca5
Lesley Lai <laisililai@live.cn>
2017-10-29 09:47:41 -0600

    Change color

--- designs.js
+++ designs.js
@@ -1,11 +1,11 @@
 // Select color input
-
+const colorInput = $("#colorPicker")[0];
 
 // Select size input
 const heightInput = $("#input_height")[0];
 const widthInput = $("#input_width")[0];
 
-let pixelCanvas = $("#pixel_canvas")[0];
+const pixelCanvas = $("#pixel_canvas")[0];
 
 // When size is submitted by the user, call makeGrid()
 $("#sizePicker").submit((e)=>{
@@ -17,12 +17,17 @@ function makeGrid() {
     const rows = heightInput.value;
     const cols = widthInput.value;
 
+    const changeColor = function(e) {
+        $(this).css("background", colorInput.value);
+    }
+
     $(".row").remove();
     for (let i = 0; i < rows; ++i) {
-        let row_elem = pixelCanvas.insertRow(i);
+        const row_elem = pixelCanvas.insertRow(i);
         row_elem.setAttribute("class", "row");
         for (let j = 0; j < cols; ++j) {
-            let cell= row_elem.insertCell(j);
+            const cell= $(row_elem.insertCell(j));
+            cell.mousedown(changeColor);
         }
     }
 
