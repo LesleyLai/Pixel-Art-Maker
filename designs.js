7a5b6b56d5180ae3b6f6b044dfc2e507b1928c9e
Lesley Lai <laisililai@live.cn>
2017-10-29 09:18:07 -0600

    Create grid

--- designs.js
+++ designs.js
@@ -1,10 +1,29 @@
 // Select color input
+
+
 // Select size input
+const heightInput = $("#input_height")[0];
+const widthInput = $("#input_width")[0];
+
+let pixelCanvas = $("#pixel_canvas")[0];
 
 // When size is submitted by the user, call makeGrid()
+$("#sizePicker").submit((e)=>{
+    makeGrid();
+    e.preventDefault();
+});
 
 function makeGrid() {
+    const rows = heightInput.value;
+    const cols = widthInput.value;
 
-// Your code goes here!
+    $(".row").remove();
+    for (let i = 0; i < rows; ++i) {
+        let row_elem = pixelCanvas.insertRow(i);
+        row_elem.setAttribute("class", "row");
+        for (let j = 0; j < cols; ++j) {
+            let cell= row_elem.insertCell(j);
+        }
+    }
 
 }
